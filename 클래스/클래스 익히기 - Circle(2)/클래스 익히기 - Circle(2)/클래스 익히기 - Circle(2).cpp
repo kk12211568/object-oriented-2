26248d3dd4b73ad78fd9eb893b5d58bb6c84313f
kk12211568 <81316934+kk12211568@users.noreply.github.com>
2022-09-21 16:45:16 +0900

    Add files via upload

---
+++ 클래스/클래스 익히기 - Circle(2)/클래스 익히기 - Circle(2)/클래스 익히기 - Circle(2).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle
+{
+private:
+	int const PI = 3.14;
+	double radius;
+
+public:
+	Circle(double radius);
+	Circle();
+	~Circle();                           //객체가 자신이 선언된 스코프를 벗어날때 자동호출됨, 생성된 반대 순서로 소멸됨(스택&큐)
+	Circle(const Circle& circle);        //말그대로 '복사 생성자'
+	void setRadius(double radius);
+	double getRadius() const;
+	double getArea() const;
+	double getPerimeter() const;
+};
+Circle::Circle(double radius)
+{
+	cout << "매개변수가 있는 생성자가 호출되었습니다." << endl;
+}
+Circle::Circle()
+{
+	cout << "기본 생성자가 호출되었습니다." << endl;            //말그대로 '기본' --> 자동으로 0.0으로 초기화 됨.
+}
+Circle::~Circle()
+{
+	cout << "소멸자가 호충되었습니다." << endl;
+}
+Circle::Circle(const Circle& circle)
+{
+	cout << "복사 생성자가 호출되었습니다." << endl;
+}
+void Circle::setRadius(double r)
+{
+	radius = r;
+}
+double Circle::getRadius() const
+{
+	return radius;
+
+}
+double Circle::getArea() const
+{
+	return radius * radius * PI;
+}
+double Circle::getPerimeter() const
+{
+	return 2 * radius * PI;
+}
+
+int main()
+{
+	Circle circle1(1.2);
+
+	Circle circle2(circle1);       //circle1의 복사 생성자인 circle2 "ㄹㅇ 복사"
+
+	Circle circle3;                //기본 생성자 circle3 --> 자동으로 0으로 초기화됨. (0 = 쓰레기값 ㅋ)
+}
---
+++ 클래스/클래스 익히기 - 직사각형/클래스 익히기 - 직사각형/클래스 익히기 - 직사각형.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+	int width;
+	int height;
+
+public:
+	Rectangle(int w, int h);
+	~Rectangle();
+	int get_width();
+	int get_height();
+	int area() const;
+	int perimeter() const;
+	bool is_square() const;
+};
+Rectangle::Rectangle(int w, int h)
+{
+	width = w;
+	height = h;
+}
+Rectangle::~Rectangle()
+{
+	cout << "직사각형이 소멸되었습니다." << endl;
+}
+int Rectangle::get_width()
+{
+	return width;
+}
+int Rectangle::get_height()
+{
+	return height;
+}
+int Rectangle::area() const
+{
+	return width * height;
+}
+int Rectangle::perimeter() const
+{
+	return 2 * width + 2 * height;
+}
+bool Rectangle::is_square() const
+{
+	if (width == height) return true;
+	else false;
+}
+
+int main()
+{
+	Rectangle rect1(10,9);
+	cout << "가로: " << rect1.get_width() << ", 세로: " << rect1.get_height() << endl;
+	cout << "넓이: " << rect1.area() << ", 둘레:" << rect1.perimeter() << endl;
+	cout << "정사각형인가요? ";
+	if (rect1.is_square() == true) cout << "Yes!";
+	else cout << "No!";
+	
+	cout << endl << endl;
+	Rectangle rect2(16, 16);
+	cout << "가로: " << rect2.get_width() << ", 세로: " << rect2.get_height() << endl;
+	cout << "넓이: " << rect2.area() << ", 둘레:" << rect2.perimeter() << endl;
+	cout << "정사각형인가요? ";
+	if (rect2.is_square() == true) cout << "Yes!";
+	else cout << "No!";
+	cout << endl << endl;
+
+
+
+}
