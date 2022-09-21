c4fd57b4a4a4ede769523729944084313f0d10f0
kk12211568 <81316934+kk12211568@users.noreply.github.com>
2022-09-21 16:52:22 +0900

    Add files via upload

---
+++ 클래스 상속/Friend 함수 익히기/Friend 함수 익히기/Friend 함수 익히기.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+class point
+{
+private:
+    int x;
+    int y;
+
+public:
+    point() {};
+    point(int x_, int y_)
+    {
+        x = x_; y = y_;
+    }
+    void print()
+    {
+        cout << "( " << x << ", " << y << " )" << endl;
+    }
+
+    point operator*(point ob) const
+    {
+        point temp;
+        temp.x = x * ob.x;
+        temp.y = y * ob.y;
+        return temp;
+    }
+    point operator*(int k) const
+    {
+        point temp;
+        temp.x = x * k;
+        temp.y = y * k;
+        return temp;
+    }
+
+    friend point operator*(int k, point& ob)
+    {
+        point temp;
+        temp.x = ob.x * k;
+        temp.y = ob.y * k;
+        return temp;
+    }
+};
+
+
+int main()
+{
+    point ob(10, 8);
+    point ob2, ob3;
+
+    ob2 = ob * 10;
+    ob2.print();
+
+    ob3 = 10 * ob;              //friend 비멤버 함수 선언 했을때만 가능 (*순서 바뀔 때*)
+    ob3.print();
+}
---
+++ 클래스 상속/연산자 오버로딩 익히기/연산자 오버로딩 익히기/연산자 오버로딩 익히기.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+    int x;
+    int y;
+
+public:
+    Point() {};
+
+    Point(int x_, int y_)
+    {
+        x = x_; y = y_;
+    }
+    void GetPoint()
+    {
+        cout << "( " << x << ", " << y << " )" << endl;
+    }
+
+    Point operator++()
+    {
+        ++x; ++y;
+        return *this;             //클래스 자기자신을 리턴할때 *this 사용
+    }
+
+    Point operator--()
+    {
+        --x; --y;
+        return *this;
+    }
+
+    Point operator+(Point ob)
+    {
+        Point temp;
+        temp.x = x + ob.x;
+        temp.y = y + ob.y;
+        return temp;
+    }
+
+    void operator!=(Point ob)
+    {
+        if (x != ob.x || y != ob.y) { cout << 'T'; }
+        else { cout << 'F'; }
+    }
+};
+int main()
+{
+    Point point1(8, 10);
+    Point point2(-10, 15);
+    Point point3(9, 10);
+
+    ++point1; point1.GetPoint();
+    ++point2; point2.GetPoint();
+
+    Point ob = point1 + point2;
+    ob.GetPoint();
+
+    point1 != point3;
+}
